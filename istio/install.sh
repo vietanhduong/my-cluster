3f11fb263a2529f29ca9ff1fc4e7c368230d64a9
Viet-Anh Duong <vietanhs0817@gmail.com>
2021-08-31 09:51:51 +0700

    Update install.sh

--- istio/install.sh
+++ istio/install.sh
@@ -5,15 +5,17 @@ set -eo pipefail
 ISTIO_VERSION=${ISTIO_VERSION:-1.9.7}
 
 # Prepare before download
-[[ -d "istio-$ISTIO_VERSION" ]] && mv "istio-$ISTIO_VERSION" "old-istio-$ISTIO_VERSION"
+if [[ -d "istio-$ISTIO_VERSION" ]]; then 
+  mv "istio-$ISTIO_VERSION" "old-istio-$ISTIO_VERSION"
+fi
 
 if ! curl -sSfL https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION sh - &> /dev/null; then 
   echo "Download istio with version $ISTIO_VERSION failed!" >&2
   exit 1
 fi
 
-# # Copy chart
-# mv "istio-$ISTIO_VERSION"/manifests/charts .charts
+# Copy chart
+mv "istio-$ISTIO_VERSION"/manifests/charts .charts
 
-# # Remove junk
-# rm -rf "istio-$ISTIO_VERSION"
+# Remove junk
+rm -rf "istio-$ISTIO_VERSION"
